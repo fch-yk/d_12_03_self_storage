876613026b426ac86fa1290a7064fcb3b801ab9d
milov52 <111310007+milov52@users.noreply.github.com>
2023-04-17 17:46:57 +0300

    Milov (#1)
    * add idea folder
    
    * Add template for main page
    
    * Fixed static path
    
    * Add func for deploy

--- storage/views.py
+++ storage/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def show_home(request):
     context = {}
-    return render(request, 'index.html', context=context)
+    template = 'storage/index.html'
+    return render(request, template, context=context)
