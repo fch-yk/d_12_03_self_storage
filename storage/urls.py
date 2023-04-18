0547c024f00aec0369de4b059498d79c58d732bf
killerarz <milov52@gmail.com>
2023-04-18 16:10:40 +0300

    Add boxed page

--- storage/urls.py
+++ storage/urls.py
@@ -8,4 +8,5 @@ app_name = 'storage'
 urlpatterns = [
     path('', views.show_home, name='index'),
     path('faq/', views.show_faq_page, name='faq'),
+    path('boxes/', views.show_boxes_page, name='boxes'),
 ]
