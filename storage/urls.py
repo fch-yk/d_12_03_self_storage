8f84ba497a9a22ad8ec2c2f810bab04f75373651
killerarz <milov52@gmail.com>
2023-04-20 12:23:20 +0300

    Add payment order

--- storage/urls.py
+++ storage/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.show_home, name='index'),
     path('faq/', views.show_faq_page, name='faq'),
     path('boxes/', views.show_boxes_page, name='boxes'),
+    path('boxes/<int:box_id>/payment', views.show_payment_page, name='payment'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
