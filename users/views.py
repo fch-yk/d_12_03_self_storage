60e396de0d27615ba6d2cdb44774e4d3bcc49419
milov52 <111310007+milov52@users.noreply.github.com>
2023-04-21 21:46:47 +0300

    Fixed user error (#30)
    * Fixed change user data
    
    * Fixed view area boxes

--- users/views.py
+++ users/views.py
@@ -14,35 +14,26 @@ class SignUp(CreateView):
     template_name = 'users/signup.html'
 
 
-def profile(request, username):
-    current_user = get_object_or_404(User, username=username)
-    orders = Order.objects.filter(customer=current_user).days_left()
-    context = {
-        'current_user': current_user,
-        'orders': orders,
-    }
+def profile(request):
+    orders = Order.objects.filter(customer=request.user).days_left()
 
     if request.POST:
         request_data = request.POST
 
-        if request_data["PASSWORD_EDIT"] == '':
-            raise Exception('Пароль не может быть пустым!')
-
         fullname = request_data["NAME_EDIT"].split()[:2]
 
-        current_user.first_name = ''
-        current_user.last_name = ''
-
-        if len(fullname) == 1:
-            current_user.first_name = fullname[0]
+        if len(fullname) == 0:
+            request.user.first_name = ''
+            request.user.last_name = ''
+        elif len(fullname) == 1:
+            request.user.first_name = fullname[0]
         else:
-            current_user.first_name, current_user.last_name = fullname
+            request.user.first_name, request.user.last_name = fullname
 
-        current_user.email = request_data["EMAIL_EDIT"]
-        current_user.phone = request_data["PHONE_EDIT"]
-        current_user.set_password(request_data["PASSWORD_EDIT"])
-        current_user.save()
-        return redirect("users:login")
+        request.user.email = request_data["EMAIL_EDIT"]
+        request.user.phone = request_data["PHONE_EDIT"]
+        request.user.save()
+        return redirect("users:profile")
 
-    return render(request, "users/profile.html", context)
+    return render(request, "users/profile.html", {'orders': orders})
 
